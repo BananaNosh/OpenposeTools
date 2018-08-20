804e634924559c0486f4ea755bb5e05e41cb118a
Jonathan Loos <jonathan.loos@web.de>
2018-08-20 10:42:09 +0200

    Add splitting and recombining of videos for better memory handling

--- Visualizer.py
+++ Visualizer.py
@@ -5,6 +5,8 @@ from types import SimpleNamespace as Namespace
 import numpy as np
 import os
 from skvideo import io
+from moviepy.editor import VideoFileClip, concatenate_videoclips
+import re
 
 
 def visualize_frame():
@@ -35,29 +37,65 @@ all_seqs = [pose_seq, face_seq, hand_seq, hand_seq]
 all_colors = [pose_colors, face_colors, hand_colors, hand_colors]
 
 
-def color_video(json_zip, vid_file, out_file, start=0, end=25000, hd=True):
+def color_video(json_zip, vid_file, out_file, hd=True, max_frames=None):
     video_capture = cv2.VideoCapture(vid_file)
-    arr = []
+    output_wihout_ext, output_type = os.path.splitext(out_file)
+    frame_count = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
+    print(frame_count)
+    colored_frames = []
     with zipfile.ZipFile(json_zip) as zip_file:
         json_files = [file for file in zip_file.namelist() if file.endswith(".json")]
         json_files.sort()
+        json_count = len(json_files)
+        frame_count = min(frame_count, json_count)
+        splitted = max_frames < frame_count
+        if splitted:
+            output_path, output_name = os.path.split(output_wihout_ext)
+            output_path = os.path.join(output_path, "splitted")
+            output_wihout_ext = os.path.join(output_path, output_name)
+            os.mkdir(output_path)
+        json_files = json_files[:frame_count]
+        video_number = 1
         for i, file_name in enumerate(json_files):
-            if not file_name.endswith(".json"):
-                print("Not a json file: {}".format(file_name))
-                continue
             ret, canvas = video_capture.read()
-            if i > 200:
-                break
             print(i)
             if canvas is None:
                 print("No canvas for file: ", file_name)
                 break
-
             written_canvas = canvas_for_frame(canvas, file_name, zip_file, hd=hd)
             canvas = cv2.addWeighted(canvas, 0.1, written_canvas, 0.9, 0)
-            arr.append(canvas[:, :, [2, 1, 0]])
-    arr = np.array(arr)
-    io.vwrite(out_file, arr)  # WRITE VIDEO
+            colored_frames.append(canvas[:, :, [2, 1, 0]])
+            if max_frames and (i + 1) % max_frames == 0 and max_frames != frame_count:
+                colored_frames = np.array(colored_frames)
+                io.vwrite(f"{output_wihout_ext}_{video_number}{output_type}", colored_frames)  # WRITE VIDEO
+                video_number += 1
+                colored_frames = []
+    if len(colored_frames) > 0:
+        colored_frames = np.array(colored_frames)
+        video_number = "_" + str(video_number) if splitted else ""
+        io.vwrite(f"{output_wihout_ext}{video_number}{output_type}", colored_frames)  # WRITE VIDEO
+
+    if splitted:
+        # noinspection PyUnboundLocalVariable
+        combine_videos(output_path, True)
+
+
+def combine_videos(path, delete=True):
+    file_names = os.listdir(path)
+    file_names.sort(key=lambda _file: [int(s) if s.isdigit() else s for s in re.split(r'(\d+)', _file)])
+    if len(file_names) == 0:
+        return
+    total_file_names = [os.path.join(path, video_name) for video_name in file_names]
+    video_clips = [VideoFileClip(video_name) for video_name in total_file_names]
+    final_clip = concatenate_videoclips(video_clips)
+    new_path, _ = os.path.split(path)
+    file_name, ext = os.path.splitext(file_names[0])
+    new_name = os.path.join(new_path, file_name[:-2] + ".mp4")
+    final_clip.write_videofile(new_name)
+    if delete:
+        for file in total_file_names:
+            os.remove(file)
+        os.rmdir(path)
 
 
 def canvas_for_frame(current_canvas, frame_json, zip_file, hd=True):
@@ -90,13 +128,13 @@ def canvas_for_frame(current_canvas, frame_json, zip_file, hd=True):
 
 
 if __name__ == '__main__':
-    path = os.path.join(".", "data")
-    video_filename = os.path.join(path, "2018-05-29_2200_US_KNBC_The_Ellen_DeGeneres_Show_672-1147.mp4")
-    json_zip = os.path.join(path, "2905_small_json.zip")
-    out_path = os.path.join(path, "test_colored_video_small.avi")
-    # video_filename = os.path.join(path, "2018-05"
+    _path = os.path.join(".", "data")
+    _video_filename = os.path.join(_path, "2018-05-29_2200_US_KNBC_The_Ellen_DeGeneres_Show_672-1147.mp4")
+    _json_zip = os.path.join(_path, "2905_small_json.zip")
+    _out_path = os.path.join(_path, "test_colored_video_small.avi")
+    # _video_filename = os.path.join(path, "2018-05"
     #                                     "-29_2200_US_KNBC_The_Ellen_DeGeneres_Show_repaired_compressed_only_video_672"
     #                                     "-1147_HD.mp4")
-    # json_zip = os.path.join(path, "2905_HD_reduced_to_-1_368.zip")
-    # out_path = os.path.join(path, "test_colored_video_hd.avi")
-    color_video(json_zip, video_filename, out_path, hd=False)
+    # _json_zip = os.path.join(path, "2905_HD_reduced_to_-1_368.zip")
+    # _out_path = os.path.join(path, "test_colored_video_hd.avi")
+    color_video(_json_zip, _video_filename, _out_path, hd=False, max_frames=500)
